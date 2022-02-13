87b461a5047c3f70656ad0b307c673e71ac8c916
thereturn932 <mburakozenn@gmail.com>
2022-02-13 16:42:45 +0300

    Adds token deposit

--- contracts/GordionTVInvestment.sol
+++ contracts/GordionTVInvestment.sol
@@ -1,6 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract GordionTVInvestment {
+    IERC20 usdte = IERC20(0xc7198437980c041c805A1EDcbA50c1Ce5db95118);
+    IERC20 usdt = IERC20(0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7);
+    IERC20 usdce = IERC20(0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664);
+    IERC20 usdc = IERC20(0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E);
+    IERC20 dai = IERC20(0xd586E7F844cEa2F87f50152665BCbc2C279D8d70);
+
     
 }
--- contracts/GordionianParticipantVoter.sol
+++ contracts/GordionianParticipantVoter.sol
@@ -6,12 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract GordionianParticipantVoter is Ownable {
-    IERC20 usdte = IERC20(0xc7198437980c041c805A1EDcbA50c1Ce5db95118);
-    IERC20 usdt = IERC20(0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7);
-    IERC20 usdce = IERC20(0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664);
-    IERC20 usdc = IERC20(0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E);
-    IERC20 dai = IERC20(0xd586E7F844cEa2F87f50152665BCbc2C279D8d70);
-
     uint256 votingID;
 
     struct Film {
